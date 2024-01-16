db969404e54924a2b5b916f308c69ae0dbcbc2bf
Sebastian <juansecapri@hotmail.com>
2024-01-15 22:10:01 -0500

    first commit

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,10 @@
+// alert('Hola Mundo');
+
+//Null y Undefined son diferentes
+
+// let nombre = prompt('Cual es tu nombre?');
+// console.log(nombre);
+// console.log( '*****' + nombre + '*****' );
+
+const seleccion = confirm('Estas seguro de borrar esto?');
+console.log(seleccion);
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,37 @@
+// console.log( miNuevoNombre + ' Jimenez' );
+
+
+// alert('Hola desde app.js');
+
+
+// console.log('Hola Mundo');
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+// console.log('%c Mis variables', 'color:yellow; font-weight: bold')
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = 'Hola de nuevo';
+
+// console.table({a, b, c, d, x });
+
+var miNombre = 'Sebastian';
+
+// let outerWidth = 1000000;
+// let outerHeight = 1500;
+
+// Let y Const no sobre escriben las variables globales
+// var sobreescribe las variables globales
+
+let miNuevoNombre = 'Sebastian Camargo';
+
+// Cualquier variable que no este inicializada va a tener el valor de undefined
+// No es recomendable usar var, por que se puede sobreescribir facilmente y no es facil de debuggear en el codigo aparte de que se puede usar antes de ser inicializada
