6d96185fcf8b8569021ebb73f7113fa344f2ed95
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-20 11:25:16 -0500

    Cambios en blackjack

--- 04-blackjack-vite/src/blackjack/usecases/crear-carta-html.js
+++ 04-blackjack-vite/src/blackjack/usecases/crear-carta-html.js
@@ -1,15 +1,15 @@
 /**
  * 
  * @param {String} carta
- * @returns {HTMLElement} Imagen de retorno
+ * @returns {HTMLImageElement} Imagen de retorno
  */
 export const crearCartaHTML = (carta) => {
 
     if (!carta) throw new Error('La carta es necesaria');
 
-    // <img class="carta" src="assets/cartas/2C.png">
     const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`; //3H, JD
+    imgCarta.src = `assets/cartas/${carta}.png`;
     imgCarta.classList.add('carta');
 
+    return imgCarta;
 }
