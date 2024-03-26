75d480f767c2450d23c95f4bb08114c721f6cda8
Sebastian Camargo <juansecapri@hotmail.com>
2024-03-26 12:14:01 -0500

    Avances seccion 10

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -5,6 +5,7 @@ import { renderTodos } from './use-cases';
 
 const ElementIds = {
     TodoList: '.todo-list',
+    NewTodoInput: '#new-todo-input',
 };
 
 /**
@@ -16,8 +17,8 @@ export const App = (elementId) => {
 
 
     const displayTodos = () => {
-        const todos = todoStore.getTodos(todoStore.getCurrentFilter());   
-        renderTodos( ElementIds.TodoList, todos); 
+        const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(ElementIds.TodoList, todos);
     }
 
     // Cuando la función se autoinvoca, se ejecuta el código que está dentro de ella
@@ -27,5 +28,41 @@ export const App = (elementId) => {
         document.querySelector(elementId).appendChild(app);
         displayTodos();
     })();
+
+    //Referencias HTML
+
+    const newDescriptionInput = document.querySelector(ElementIds.NewTodoInput);
+    const todoListUL = document.querySelector(ElementIds.TodoList);
+    //Listeners
+
+    newDescriptionInput.addEventListener('keyup', (event) => {
+        if (event.keyCode != 13) return;
+        if (event.target.value.trim().length === 0) return;
+
+        todoStore.addTodo(event.target.value);
+        displayTodos();
+        newDescriptionInput.value = '';
+    });
+
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        const dataID = element.getAttribute('data-id');
+        // console.log(dataID);
+        todoStore.toggleTodo(dataID);
+        displayTodos();
+    });
+
+    todoListUL.addEventListener('click', (event) => {
+
+        const isDestroy = event.target.className === 'destroy';
+        const element = event.target.closest('[data-id]');
+        const dataID = element.getAttribute('data-id');
+
+        if (!element || !isDestroy) return;
+
+        todoStore.deleteTodo(dataID);
+        displayTodos();
+        
+    });
 
 } 
