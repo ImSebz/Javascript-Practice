14456b601a6e6c47fbc31fdf7498985737d3e7c6
Sebastian Camargo <juansecapri@hotmail.com>
2024-04-04 09:03:34 -0500

    Avances sección 11

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -6,13 +6,57 @@ import { heroes } from "../data/heroes";
  */
 export const promiseComponent = (element) => {
 
-    const renderHero = ( hero ) => {
+    const renderHero = (hero) => {
         element.innerHTML = hero.name;
     };
 
-    const id1 = '5d86371fd55e2e2a30fe1ccb2';
+    const renderTwoHeroes = (hero1, hero2) => {
+        element.innerHTML = `
+            <h3>${hero1.name} and ${hero2.name}</h3>
+        `;
+    }
 
-    findHero(id1);
+    const renderError = (error) => {
+        element.innerHTML = `
+            <h3 style="color:#f00;">${error}</h3>
+        `;
+    };
+
+    const id1 = '5d86371fd55e2e2a30fe1ccb2';
+    const id2 = '5d86371f2343e37870b91ef12';
+
+    let hero1;
+
+    //! Promise All
+    Promise.all([
+        findHero(id1),
+        findHero(id2),
+    ])
+        .then(([hero1, hero2]) => renderTwoHeroes(hero1, hero2))
+        .catch(renderError);
+
+
+    //! Forma 2
+    // findHero(id1)
+    //     .then(hero => {
+    //         hero1 = hero;
+    //         return findHero(id2);
+    //     }).then(hero2 => {
+    //         renderTwoHeroes(hero1, hero2);
+    //     })
+    //     .catch(renderError);
+
+    //! Forma 1
+    // findHero(id1)
+    //     .then(hero1 => {
+    //         findHero(id2)
+    //             .then(hero2 => {
+    //                 renderTwoHeroes(hero1, hero2);
+    //             })
+    //             .catch(renderError);
+    //     })
+    //     .catch(renderError);
+    //Promise Hell
 
 };
 
@@ -21,22 +65,22 @@ export const promiseComponent = (element) => {
  * @param {String} id 
  * @returns {Promise}
  */
-const findHero = ( id ) => {
+const findHero = (id) => {
 
-    return new Promise( ( resolve, reject ) => {
+    return new Promise((resolve, reject) => {
         console.log('Searching hero...');
-        
-        const hero = heroes.find( hero => hero.id === id );  
-        
-        if ( hero ) {
-            resolve( hero );
+
+        const hero = heroes.find(hero => hero.id === id);
+
+        if (hero) {
+            resolve(hero);
+            console.log('Hero found!');
             return;
         }
-        else {
-            reject(`Hero with id ${id} not found`);
-        }
 
-    } );
+        reject(`Hero with id ${id} not found`);
+
+    });
 
 
 }
