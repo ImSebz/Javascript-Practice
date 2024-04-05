ba8168ef482940b4580c56e01513786c355a96b2
Sebastian Camargo <juansecapri@hotmail.com>
2024-04-05 11:25:29 -0500

    Fin sección13

--- 07-http-app/src/breakingbad/breakingbad-app.js
+++ 07-http-app/src/breakingbad/breakingbad-app.js
@@ -1,9 +1,44 @@
 /**
  * 
+ * @returns {Object} quote information
+ */
+
+const fetchQuote = async () => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await res.json();
+    return data[0];
+
+
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = ( element ) => {
-    document.querySelector('#app-title').innerHTML = 'Breaking Bad App'
-    element.innerHTML = 'Loading...'
+export const BreakingbadApp = async (element) => {
+    document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
+    element.innerHTML = 'Loading...';
 
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h4');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerHTML = 'Next Quote';
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = `- ${data.author}`;
+
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    nextQuoteButton.addEventListener('click', async() => {
+        element.innerHTML = 'Loading...';
+        const quote = await fetchQuote();
+        renderQuote(quote);
+
+    });
+
+    fetchQuote()
+        .then(renderQuote);
 };
