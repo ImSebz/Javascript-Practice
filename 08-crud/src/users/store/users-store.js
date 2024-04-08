d2173d79b8d20749bc6b79ea9cfb1e87fb11420a
Sebastian Camargo <juansecapri@hotmail.com>
2024-04-08 11:49:11 -0500

    Avances sección 13

---
+++ 08-crud/src/users/store/users-store.js
@@ -0,0 +1,43 @@
+import { User } from "../models/user";
+import { loadUsersByPage } from "../use-cases/load-users-by-page";
+
+const state = {
+    currentPage: 0,
+    users: [],
+}
+
+const loadNextPage = async () => {
+    const users = await loadUsersByPage(state.currentPage + 1);
+    if (users.length === 0) return;
+
+    state.currentPage += 1;
+    state.users = users;
+}
+
+const loadPreviousPage = async () => {
+    throw new Error('Not implemented');
+}
+
+const onUserChanged = () => {
+    throw new Error('Not implemented');
+}
+
+const reloadPage = () => {
+    throw new Error('Not implemented');
+}
+
+export default {
+    loadNextPage,
+    loadPreviousPage,
+    onUserChanged,
+    reloadPage,
+    /**
+     * @returns {User[]}
+     */
+    getUsers: () => [...state.users],
+    /**
+     * 
+     * @returns {Number}
+     */
+    getCurrentPage: () => state.currentPage,
+}
